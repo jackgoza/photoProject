91803587216f42b8adf1e9552eb2b1f7ccf4f2a8
Jack G <jgkp9@umkc.edu>
2016-09-18 17:47:26 -0500

    Indexing, skeleton of image serial
    Add back in 1.jpg in order to meet Homework constraints.

--- src/photoViewer.java
+++ src/photoViewer.java
@@ -1,6 +1,8 @@
 
-
+import java.util.*;
+import java.io.*;
 import java.awt.*;
+import java.awt.image.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
@@ -11,43 +13,48 @@ import javax.swing.event.*;
  *
  * @author jack
  */
-public class photoViewer extends JFrame implements ActionListener {
+public class photoViewer extends JFrame implements ActionListener, Serializable {
     
     JButton nextButton = null;
     JButton prevButton = null;
     JTextField currPageText = null;
     JTextArea totalPageText = null;
     JLabel imageLabel = null;
+    ArrayList<BufferedImage> images = null;
     
-    int imageNumber = 0;
+    int imageNumber = 1;
     
-    public photoViewer(String title){
-        super(title);
+    public photoViewer(String title) // Constructor
+    {
         
-        JButton incrementButton = new JButton("increment");
-        // Register this class to handle events from
-        // the button.
-        incrementButton.addActionListener(this);
+        // Set title to string passed in
+        super(title);
         
         Container contentPane = getContentPane();
         
+        // Create button to hold image
         imageLabel = new JLabel("", SwingConstants.CENTER);
         JScrollPane scrollPane = new JScrollPane(imageLabel);
         
+        // This is temporary
         ImageIcon image = new ImageIcon("1.jpg");
         imageLabel.setIcon(image);
         
         contentPane.add(scrollPane);
         
+        // Create bottom panel to hold buttons, numbers
         Container bottomPanel = new JPanel();
         FlowLayout fl = new FlowLayout(FlowLayout.LEFT,5, 20);
         bottomPanel.setLayout(fl);
         
-        currPageText = new JTextField("0");
-        totalPageText = new JTextArea("0");
+        // Create buttons and text fields
+        currPageText = new JTextField("1");
+        totalPageText = new JTextArea("5");
         prevButton = new JButton("<Prev");
-        prevButton.addActionListener(this);
         nextButton = new JButton("Next>");
+        
+        // Init action listeners to check for button presses
+        prevButton.addActionListener(this);
         nextButton.addActionListener(this);
         
         bottomPanel.add(currPageText);
@@ -59,16 +66,22 @@ public class photoViewer extends JFrame implements ActionListener {
 
     }
     
+    private void importPhotos(String cwd){
+        
+    }
+    
     @Override
     public void actionPerformed(ActionEvent evt) {
         System.out.println("ACTION");
         if (evt.getSource() == nextButton){
-            imageNumber++;
-            currPageText.setText(Integer.toString(imageNumber));
-            System.out.println(imageNumber);
+            if (imageNumber < 5){
+                imageNumber++;
+                currPageText.setText(Integer.toString(imageNumber));
+            }
+
         }
         else if (evt.getSource() == prevButton){
-            if(imageNumber > 0){
+            if(imageNumber > 1){
                 imageNumber--;
                 currPageText.setText(Integer.toString(imageNumber));
 
