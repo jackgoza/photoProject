fcdd9949e0cb3b4157d2f10a60a52f0d1da08d92
Jack G <jgkp9@umkc.edu>
2016-09-20 14:44:33 -0500

    Fixed button positioning and added zoom scroller
    Buttons now position themselves in the bottom left, zoom in the bottom
    right

--- src/photoViewer.java
+++ src/photoViewer.java
@@ -33,35 +33,58 @@ public class photoViewer extends JFrame implements ActionListener, Serializable 
         Container contentPane = getContentPane();
         
         // Create button to hold image
+        // (TODO: Bound the scroll pane on init)
         imageLabel = new JLabel("", SwingConstants.CENTER);
         JScrollPane scrollPane = new JScrollPane(imageLabel);
         
-        // This is temporary
+        // This is temporary (TODO: INIT TO IMAGE 1)
         ImageIcon image = new ImageIcon("1.jpg");
         imageLabel.setIcon(image);
         
         contentPane.add(scrollPane);
         
         // Create bottom panel to hold buttons, numbers
+
         Container bottomPanel = new JPanel();
-        FlowLayout fl = new FlowLayout(FlowLayout.LEFT,5, 20);
-        bottomPanel.setLayout(fl);
-        
+        Container bottomPanelWest = new JPanel();
+        Container bottomPanelEast = new JPanel();
+        FlowLayout flleft = new FlowLayout(FlowLayout.LEFT,5, 20);
+        FlowLayout flright = new FlowLayout(FlowLayout.RIGHT,5, 20);
+        bottomPanelWest.setLayout(new FlowLayout(FlowLayout.LEFT));
+        bottomPanelEast.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.LINE_AXIS));
+
         // Create buttons and text fields
         currPageText = new JTextField("1");
         totalPageText = new JTextArea("5");
         prevButton = new JButton("<Prev");
         nextButton = new JButton("Next>");
+        prevButton.setEnabled(false);
         
+        // Create zoom slider (TO BE IMPLIMENTED)
+        JSlider zoomSlide= new JSlider(JSlider.HORIZONTAL,0,100,10);
+        zoomSlide.addChangeListener(new ChangeListener() {
+        public void stateChanged(ChangeEvent e) {
+            System.out.println("Value : " 
+            + ((JSlider)e.getSource()).getValue());
+         }
+      });
+    
         // Init action listeners to check for button presses
         prevButton.addActionListener(this);
         nextButton.addActionListener(this);
         
-        bottomPanel.add(currPageText);
-        bottomPanel.add(totalPageText);
-        bottomPanel.add(prevButton);
-        bottomPanel.add(nextButton);
+        bottomPanelWest.add(currPageText);
+        bottomPanelWest.add(totalPageText);
+        bottomPanelWest.add(prevButton);
+        bottomPanelWest.add(nextButton);
+        bottomPanelEast.add(zoomSlide);
         
+
+        bottomPanel.add(bottomPanelWest);
+        bottomPanel.add(Box.createHorizontalGlue());
+        bottomPanel.add(bottomPanelEast);
+
         contentPane.add(bottomPanel,BorderLayout.SOUTH);
 
     }
@@ -77,6 +100,9 @@ public class photoViewer extends JFrame implements ActionListener, Serializable 
             if (imageNumber < 5){
                 imageNumber++;
                 currPageText.setText(Integer.toString(imageNumber));
+                if (imageNumber == 2){
+                    prevButton.setEnabled(true);
+                }
             }
 
         }
@@ -84,8 +110,16 @@ public class photoViewer extends JFrame implements ActionListener, Serializable 
             if(imageNumber > 1){
                 imageNumber--;
                 currPageText.setText(Integer.toString(imageNumber));
-
+                if (imageNumber == 4){
+                    nextButton.setEnabled(true);
+                }
             }
+        }
+        if (imageNumber <= 1){
+            prevButton.setEnabled(false);
+        }
+        else if (imageNumber >= 5){
+            nextButton.setEnabled(false);
         }
     }
 
